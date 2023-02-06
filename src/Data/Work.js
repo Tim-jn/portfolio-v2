b3f509ce81f5813091d1c0bc86b2c68e240088b2
Tim <Tim280696@hotmail.com>
2023-02-06 15:06:04 +0100

    create data works

---
+++ src/Data/Work.js
@@ -0,0 +1,29 @@
+export const data = [
+  {
+    id: 1,
+    name: "Rimarok.com",
+    date: "Aug, 2022",
+    live: "https://rimarok.com/",
+    source: "https://github.com/Skaant/rimarok",
+  },
+  {
+    id: 2,
+    name: "Shortly",
+    date: "Mai, 2022",
+    live: "https://timjn-shortly.netlify.app",
+    source: "https://github.com/Tim-jn/shortly",
+  },
+  {
+    id: 3,
+    name: "Jeanmartth.be",
+    date: "Mar, 2022",
+    live: "https://jeanmartth.be",
+  },
+  {
+    id: 4,
+    name: "Fylo",
+    date: "Mar, 2022",
+    live: "https://timjn-fylo.netlify.app/",
+    source: "https://github.com/Tim-jn/Fylo",
+  },
+];
