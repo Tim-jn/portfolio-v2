16a4422fae2a3f94bc44a49044c76b05fc9fb8d9
Tim <Tim280696@hotmail.com>
2023-02-06 16:50:47 +0100

    minor update

--- src/Components/Title/Title.tsx
+++ src/Components/Title/Title.tsx
@@ -4,7 +4,7 @@ export default function Title() {
   return (
     <header>
       <h1 className="title">Tim Jeanmart</h1>
-      <h2 className="subtitle">Développeur Front-End</h2>
+      <h2 className="subtitle">Développeur web</h2>
     </header>
   );
 }
