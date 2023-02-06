79dee20a25b2a69675352bf67de2b325dcbfcb68
Tim <Tim280696@hotmail.com>
2023-02-06 16:51:18 +0100

    update display

--- src/Pages/Index/Index.tsx
+++ src/Pages/Index/Index.tsx
@@ -13,56 +13,68 @@ export default function Index() {
 
   return (
     <main className="main-structure">
-      <Title />
-      <nav>
-        <ul className="sidebar-links">
-          <li>
-            <button
-              className={content === 1 ? "sidebar-link active" : "sidebar-link"}
-              onClick={() => setContent(1)}
-            >
-              à propos
-            </button>
-          </li>
-          <li>
-            <button
-              className={content === 2 ? "sidebar-link active" : "sidebar-link"}
-              onClick={() => setContent(2)}
-            >
-              compétences
-            </button>
-          </li>
-          <li>
-            <button
-              className={content === 3 ? "sidebar-link active" : "sidebar-link"}
-              onClick={() => setContent(3)}
-            >
-              derniers projets
-            </button>
-          </li>
-          <li>
-            <button
-              className={content === 4 ? "sidebar-link active" : "sidebar-link"}
-              onClick={() => setContent(4)}
-            >
-              contact
-            </button>
-          </li>
-        </ul>
-      </nav>
-      <section className="content">
-        {content === 1 ? (
-          <About />
-        ) : content === 2 ? (
-          <Skills />
-        ) : content === 3 ? (
-          <Work />
-        ) : content === 4 ? (
-          <Contact />
-        ) : (
-          ""
-        )}
-      </section>
+      <div className="left-content">
+        <Title />
+        <nav>
+          <ul className="sidebar-links">
+            <li>
+              <button
+                className={
+                  content === 1 ? "sidebar-link active" : "sidebar-link"
+                }
+                onClick={() => setContent(1)}
+              >
+                à propos
+              </button>
+            </li>
+            <li>
+              <button
+                className={
+                  content === 2 ? "sidebar-link active" : "sidebar-link"
+                }
+                onClick={() => setContent(2)}
+              >
+                compétences
+              </button>
+            </li>
+            <li>
+              <button
+                className={
+                  content === 3 ? "sidebar-link active" : "sidebar-link"
+                }
+                onClick={() => setContent(3)}
+              >
+                derniers projets
+              </button>
+            </li>
+            <li>
+              <button
+                className={
+                  content === 4 ? "sidebar-link active" : "sidebar-link"
+                }
+                onClick={() => setContent(4)}
+              >
+                contact
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="right-content">
+        <section className="content">
+          {content === 1 ? (
+            <About />
+          ) : content === 2 ? (
+            <Skills />
+          ) : content === 3 ? (
+            <Work />
+          ) : content === 4 ? (
+            <Contact />
+          ) : (
+            ""
+          )}
+        </section>
+      </div>
     </main>
   );
 }
