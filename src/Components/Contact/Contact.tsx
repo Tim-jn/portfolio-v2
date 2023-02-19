dbd004246c2452d49da0bd8e8e29b04697916e39
Tim <Tim280696@hotmail.com>
2023-02-19 16:03:18 +0100

    create contact component

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -8,6 +8,37 @@ export default function Contact({ title }: Props) {
   return (
     <div className="contact-section">
       <div className="section-title">{title}</div>
+      <form
+        className="contact-form"
+        name="contact"
+        method="POST"
+        data-netlify="true"
+      >
+        <p className="text-contact">
+          Si vous avez une question, n'hésitez pas à me contacter en
+          remplissant les champs ci-dessous. Je vous répondrais dès que possible
+          !
+        </p>
+
+        <p>
+          <label>
+            nom : <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email : <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message : <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Envoyer</button>
+        </p>
+      </form>
     </div>
   );
 }
