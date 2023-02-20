3230e74a382fd2918690b1876c2e4683221eb2dd
Tim <Tim280696@hotmail.com>
2023-02-20 17:09:00 +0100

    update style

--- src/Components/Title/Title.tsx
+++ src/Components/Title/Title.tsx
@@ -1,9 +1,18 @@
 import "./Title.scss";
+import { useMediaQuery } from "react-responsive";
 
 export default function Title() {
+  const isSmallerScreen = useMediaQuery({ query: "(min-width: 569px)" });
   return (
     <header>
-      <h1 className="title">Tim Jeanmart</h1>
+      {isSmallerScreen ? (
+        <h1 className="title">Tim Jeanmart</h1>
+      ) : (
+        <>
+          <h1 className="title title-mobile">Tim</h1>
+          <h1 className="title title-mobile">Jeanmart</h1>
+        </>
+      )}
       <h2 className="subtitle">Développeur web</h2>
     </header>
   );
